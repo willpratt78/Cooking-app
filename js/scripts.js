798b704cb9287ecdb3d27b0e7eb4690cbaf207cc
William Pratt <willpratt78@gmail.com>
2020-03-23 16:24:04 -0700

    debugging js script

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  // $(button).click(function() {
+var Grams = function(number1) {
+  return number1*.03527
+};
+
+var number1 = parseInt(prompt(Convert grams to ounces));
+var result = Grams(number1); 
+alert("result");
+  });
+});
+
